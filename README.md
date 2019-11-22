ad8a5bcaecf07be53a65dffd001754c58afc2a63
wizard <icewizardfn@gmail.com>
2019-11-22 14:10:10 -0600

    worked alot on requirements

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # BakerBrosBARBEQUE
  THIS IS OUR NEW SITE, DISREGARD OLD BAKERBROS
+home page named index.html
+all lowercase
+
+css file named style.css
+
+only 1 css file (need to condense that a bit)
+
+Java - all of it  needs to go main.js
+
+Comments should be used to divide the diffrent parts/containers
+
+
+
+ALL STYLES IN STYLES.CSS FILE
+ 
+Naming should be consistent, java should always be lowerCamalSpace
+ 
+ 
--- js/main.js
+++ js/main.js
@@ -280,28 +280,8 @@ AOS.init({
 	});
 	$('#book_time').timepicker();
 
-        //Menu Page Button
-	mybutton = document.getElementById("myBtn");
-
-
-	window.onscroll = function () {
-		scrollFunction()
-	};
-
-	function scrollFunction() {
-		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-			mybutton.style.display = "block";
-		} else {
-			mybutton.style.display = "none";
-		}
-	}
-
-
-	function topFunction() {
-		document.body.scrollTop = 0; // For Safari
-		document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-	}
-
 
 
 })(jQuery);
+
+
