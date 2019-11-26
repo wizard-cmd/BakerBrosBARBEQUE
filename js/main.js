9c4fd3f79afa641930b68926830e466b191e3011
wizard <icewizardfn@gmail.com>
2019-11-26 14:08:36 -0600

    oh yuh

--- js/main.js
+++ js/main.js
@@ -17,6 +17,8 @@ AOS.init({
 	});
 
 
+	
+
 	var fullHeight = function () {
 
 		$('.js-fullheight').css('height', $(window).height());
@@ -284,4 +286,23 @@ AOS.init({
 
 })(jQuery);
 
+//Menu Page Button
+mybutton = document.getElementById("myBtn");
 
+window.onscroll = function () {
+	scrollFunction()
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
+}
+
+
+function topFunction() {
+	document.body.scrollTop = 730; // For Safari
+	document.documentElement.scrollTop = 730; // For Chrome, Firefox, IE and Opera
+}
